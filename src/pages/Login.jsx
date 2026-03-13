1f693fba2f98e4a51df909bdd9a6ec2ea976ba8d
ANURAG TOMAR <anuragtomar19@ANURAGs-MacBook-Pro-3.local>
2026-03-13 21:02:17 +0530

    Setup TailwindCSS for styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,9 @@
 function Login() {
   return (
-    <div>
-      <h2>Login Page</h2>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Login Page
+      </h1>
     </div>
   );
 }
