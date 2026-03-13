ee9bcb06ce6f6de426ecfa4ced0b755c86d650af
ANURAG TOMAR <anuragtomar19@ANURAGs-MacBook-Pro-3.local>
2026-03-13 20:54:45 +0530

    Setup React Router and basic page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import TeacherDashboard from "./pages/TeacherDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
+import Submissions from "./pages/Submissions";
+
 function App() {
   return (
-    <div>
-      <h1>Assignment Portal</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/teacher" element={<TeacherDashboard />} />
+
+        <Route path="/student" element={<StudentDashboard />} />
+
+        <Route path="/teacher/submissions/:assignmentId" element={<Submissions />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Submissions.jsx
@@ -0,0 +1,9 @@
+function Submissions() {
+  return (
+    <div>
+      <h2>Submissions Page</h2>
+    </div>
+  );
+}
+
+export default Submissions;
